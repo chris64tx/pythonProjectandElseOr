3f8092a68da0f80cbb04a916a2dde8acfa7af199
chris64tx <56325829+chris64tx@users.noreply.github.com>
2021-05-15 19:39:09 -0500

    Initial commit

---
+++ andElseOr.py
@@ -0,0 +1,5 @@
+myColor = input('Please input your Color:' )
+if myColor == 'Red' or myColor == 'red' or myColor == 'RED':
+    print('Your color is Red')
+if myColor != 'Red' and myColor != 'red' and myColor != 'RED':
+    print('Your color is NOT Red')
